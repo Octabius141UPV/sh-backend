2f882a240eec582b8855d46e52d16bb9105c4ffb
Raúl Fortea Ibáñez <133568718+Octabius141UPV@users.noreply.github.com>
2025-03-04 10:38:22 +0100

    First commit

---
+++ src/test/java/com/api/solidarityhub/shbackend/ShBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.solidarityhub.shbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
